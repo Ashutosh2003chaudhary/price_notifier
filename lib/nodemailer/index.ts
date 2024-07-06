fc62af57a6cf2df00b542cfb15d0d824e204affb
Ashutosh <ashutosh2003chaudhary@gmail.com>
2024-07-06 10:02:42 +0530

    first commit

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,21 +1,23 @@
-"use server"
+"use server";
 
 import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const Notification = {
   WELCOME: 'WELCOME',
   CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
   LOWEST_PRICE: 'LOWEST_PRICE',
   THRESHOLD_MET: 'THRESHOLD_MET',
-}
+};
 
 export async function generateEmailBody(
   product: EmailProductInfo,
   type: NotificationType
-  ) {
+) {
   const THRESHOLD_PERCENTAGE = 40;
-  // Shorten the product title
   const shortenedTitle =
     product.title.length > 20
       ? `${product.title.substring(0, 20)}...`
@@ -31,12 +33,12 @@ export async function generateEmailBody(
         <div>
           <h2>Welcome to PriceWise 🚀</h2>
           <p>You are now tracking ${product.title}.</p>
-          <p>Here's an example of how you'll receive updates:</p>
+          
           <div style="border: 1px solid #ccc; padding: 10px; background-color: #f8f8f8;">
             <h3>${product.title} is back in stock!</h3>
             <p>We're excited to let you know that ${product.title} is now back in stock.</p>
             <p>Don't miss out - <a href="${product.url}" target="_blank" rel="noopener noreferrer">buy it now</a>!</p>
-            <img src="https://i.ibb.co/pwFBRMC/Screenshot-2023-09-26-at-1-47-50-AM.png" alt="Product Image" style="max-width: 100%;" />
+            // <img src="https://i.ibb.co/pwFBRMC/Screenshot-2023-09-26-at-1-47-50-AM.png" alt="Product Image" style="max-width: 100%;" />
           </div>
           <p>Stay tuned for more updates on ${product.title} and other products you're tracking.</p>
         </div>
@@ -81,27 +83,28 @@ export async function generateEmailBody(
 }
 
 const transporter = nodemailer.createTransport({
-  pool: true,
   service: 'hotmail',
-  port: 2525,
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
-    user: 'javascriptmastery@outlook.com',
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
   maxConnections: 1
-})
+});
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
   const mailOptions = {
-    from: 'javascriptmastery@outlook.com',
-    to: sendTo,
-    html: emailContent.body,
+    from: process.env.EMAIL_USER,
+    to: sendTo.join(','),
     subject: emailContent.subject,
-  }
+    html: emailContent.body,
+  };
 
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if(error) return console.log(error);
-    
+  try {
+    const info = await transporter.sendMail(mailOptions);
     console.log('Email sent: ', info);
-  })
-}
+  } catch (error) {
+    console.error('Error sending email: ', error);
+  }
+};
